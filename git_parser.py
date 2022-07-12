e602037cb09adb2eb6cec194dbcdb2330ca9413b
Nasr Saab <nsaab@nvidia.com>
2022-07-12 16:16:11 +0300

    Work with Excel file I user excel file to write all the commits

--- git_parser.py
+++ git_parser.py
@@ -7,29 +7,53 @@ from datetime import datetime
 from git import Commit
 from pydriller import Repository
 from pydriller.domain.commit import ModificationType
+import xlsxwriter as xw
 import pandas as pd
 
-
-firstDay = datetime(2022, 5, 1)
-toDay = datetime.now()
-
-Row_number = 0
-Col_number = 0
+wb_workbook = xw.Workbook("book1233.xlsx")
+ws_worksheet = wb_workbook.add_worksheet(name="Git_commit")
 
 
-url = [r"C:\Users\a\Downloads\my-Github\Git_course"]
-url2 = [r"C:\Users\a\Downloads\my-Github\Our_Project"]
-url3 = ["https://github.com/nasrsaab/myFiles.git"]
-url4 = ["https://github.com/nasrsaab/Git_course.git"]
+def git_commit(url_webset):
+    '''Function to git commit from the web sit to the xlsx file...'''
 
-for commit in Repository(path_to_repo=url, since=firstDay, to=toDay).traverse_commits():
-    for modified_file in commit.modified_files:
-        if Row_number != 5:
-            df1 = pd.DataFrame([modified_file.filename, commit.msg, commit.author_date, commit.hash],
-                               index=[0, 1, 2, 3, 4], columns=['File Name', 'Commit msg', 'Commit Date', 'hash'])
-            Row_number += 1
-        else:
-            break
+    first_day = datetime(2022, 5, 1)
+    to_day = datetime.now()
+
+    row_number = 0
+    col_number = 0
+
+    bold = wb_workbook.add_format({'bold': True})
+    italic = wb_workbook.add_format(dict(italic=True))
+
+    ws_worksheet.write(row_number, col_number, 'File Name', bold)
+    ws_worksheet.write(row_number, col_number+1, 'Commit msg', bold)
+    ws_worksheet.write(row_number, col_number+2, 'Commit Date', bold)
+    ws_worksheet.write(row_number, col_number+3, 'hash commit', bold)
+
+    row_number += 1
+   # url = [r"C:\Users\a\Downloads\my-Github\Git_course"]
+    # url2 = [r"C:\Users\a\Downloads\my-Github\Our_Project"]
+    # url3 = ["https://github.com/nasrsaab/myFiles.git"]
+    # url4 = ["https://github.com/nasrsaab/Git_course.git"]
+
+    for commit in Repository(path_to_repo=url_webset, since=first_day, to=to_day).traverse_commits():
+        for modified_file in commit.modified_files:
+            ws_worksheet.write(row_number, col_number,
+                               modified_file.filename, italic)
+            ws_worksheet.write(row_number, col_number+1, commit.msg)
+            ws_worksheet.write(row_number, col_number+2,
+                               str(commit.author_date))
+            ws_worksheet.write(row_number, col_number+3, commit.hash)
+            row_number += 1
+
+    wb_workbook.close()
 
 
-print(df1)
+'''open the xlsx file and put the data in dataframe '''
+df_dataframe = pd.read_excel("book1233.xlsx")
+df_dataframe.to_csv('book1233.csv')
+df_dataframe = pd.read_csv('book1233.csv')
+
+# print the dataframe
+print(df_dataframe)
