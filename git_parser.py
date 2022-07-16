9c6aae6087fcc0b44545d65e53d4d871812cd1cd
Nasr Saab <nsaab@nvidia.com>
2022-07-16 10:24:07 +0300

    Use argparse and options to work with flags --output_file = Path on the computer to save the created excel --repository  = Path to the repository I want the script to parse  run command as follow: 'python git_parser.py --output_file <File name>  --repository <Repo>'

--- git_parser.py
+++ git_parser.py
@@ -3,25 +3,32 @@ git a commit from the github or from the company server and set the commit in th
 Excel file
 '''
 from datetime import datetime
-
 from pydriller import Repository
 from pydriller.domain.commit import ModificationType
 import xlsxwriter as xw
-#import pandas as pd
+import argparse
+
+parser= argparse.ArgumentParser(description='git_parser commit from GitHub')
+parser.add_argument('-o','--output_file', type=str,metavar='',required=True,
+                    help='Path on the computer to same created Excel file')
+parser.add_argument('-r','--repository', type=str,metavar='',required=True,
+                    help='Path on the repository I want the script to pase')
+args=parser.parse_args()
 
 
-wb = xw.Workbook("Excel_File1.xlsx")
-ws = wb.add_worksheet(name="Git_commit")
-
-firstDay = datetime(2022, 5, 1)
-toDay = datetime.now()
 
 
-bold = wb.add_format({'bold': True})
-italic = wb.add_format(dict(italic=True))
+def show_commit(output_file,repository):
 
+    wb = xw.Workbook(output_file+"\outputExcel_file.xlsx")
+    ws = wb.add_worksheet(name="parser_git_commit")
 
-def show_commit(url):
+    firstDay = datetime(2022, 5, 1)
+    toDay = datetime.now()
+
+    bold = wb.add_format({'bold': True})
+    italic = wb.add_format(dict(italic=True))
+
     Row_number = 0
     Col_number = 0
 
@@ -32,7 +39,7 @@ def show_commit(url):
 
     Row_number += 1
 
-    for commit in Repository(path_to_repo=url, since=firstDay, to=toDay).traverse_commits():
+    for commit in Repository(path_to_repo=repository, since=firstDay, to=toDay).traverse_commits():
         for modified_file in commit.modified_files:
             ws.write(Row_number, Col_number, modified_file.filename, italic)
             ws.write(Row_number, Col_number + 1, commit.msg)
@@ -43,4 +50,5 @@ def show_commit(url):
 
     wb.close()
 
-
+if __name__=='__main__':
+    show_commit(args.output_file, args.repository)
