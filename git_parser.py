f2378be0fbe8980f6311ece822e64510d19bf1f2
Nasr Saab <nsaab@nvidia.com>
2022-07-16 10:24:07 +0300

    Use argparse and options to work with flags
    usage: parser_module.py [-h] [-o OUTPUT_FILE] -r  [-d SHOW_LATS_DAYS]
    
    Positional arguments:
    -r, --repository     Set the repository path you want to parse it
    
    Optional arguments:
    -h, --help           show this help message and exit
    -o, --output_file    OUTPUT_FILE   Set a local computer path to create an Excel file. if not
    provided - create the Excel file in the same project folder.
    -d, --show_lats_days SHOW_LATS_DAYS Show me only commits from the last 'X' days.if not
    provided - show all commits In the repository.
    
    write all the commits in the excel file
    
    No duplicate hash commit and remove a duplicated hash and set the differnt files in the same
    row in the excel file and cut the hash commit and take just the first 12
    character

--- git_parser.py
+++ git_parser.py
@@ -2,45 +2,87 @@
 git a commit from the github or from the company server and set the commit in the
 Excel file
 '''
-from datetime import datetime
-
+from datetime import date, timedelta,datetime
 from pydriller import Repository
 from pydriller.domain.commit import ModificationType
 import xlsxwriter as xw
 #import pandas as pd
 
 
-wb = xw.Workbook("Excel_File1.xlsx")
-ws = wb.add_worksheet(name="Git_commit")
 
-firstDay = datetime(2022, 5, 1)
-toDay = datetime.now()
+def show_commit(args):
+
+    #Create a output file
+    if args.output_file:
+        #Choose a new local path
+        Outputdata = args.output_file +"\outputExcel_file.xlsx"
+        wb = xw.Workbook(Outputdata)
+        print("\nExcel file created in: " + args.output_file + "\outputExcel_file.xlsx")
+    else:
+        #Set in a same arg_parser.py path
+        Outputdata = "outputExcel_file.xlsx"
+        wb = xw.Workbook(Outputdata)
+        print("\nExcel file created in: local_python_file_path\outputExcel_file.xlsx")
+
+    #Set worksheet name
+    ws = wb.add_worksheet(name="parser_git_commit")
+
+    #Add worksheet font format
+    bold = wb.add_format({'bold': True})
+    italic = wb.add_format(dict(italic=True))
+
+    #Calculate date
+    to_day = datetime.now().today()
+
+    if args.show_lats_days:
+
+        first_day = to_day - timedelta(days=args.show_lats_days)
+        print(first_day)
+        print(to_day)
+
+    else:
+        first_day = datetime.min
+        print(to_day)
 
 
-bold = wb.add_format({'bold': True})
-italic = wb.add_format(dict(italic=True))
-
-
-def show_commit(url):
+    print("Please wait...")
     Row_number = 0
     Col_number = 0
 
-    ws.write(Row_number, Col_number, 'File Name', bold)
-    ws.write(Row_number, Col_number + 1, 'Commit msg', bold)
+    ws.write(Row_number, Col_number, 'hash commit', bold)
+    ws.write(Row_number, Col_number + 1, 'Commit subject', bold)
     ws.write(Row_number, Col_number + 2, 'Commit Date', bold)
-    ws.write(Row_number, Col_number + 3, 'hash commit', bold)
+    ws.write(Row_number, Col_number + 3, 'File Name', bold)
 
-    Row_number += 1
+    hash_commit = 'xxxx'
 
-    for commit in Repository(path_to_repo=url, since=firstDay, to=toDay).traverse_commits():
+    for commit in Repository(path_to_repo=args.repository, since=first_day, to=to_day).traverse_commits():
         for modified_file in commit.modified_files:
-            ws.write(Row_number, Col_number, modified_file.filename, italic)
-            ws.write(Row_number, Col_number + 1, commit.msg)
-            ws.write(Row_number, Col_number + 2, str(commit.author_date))
-            ws.write(Row_number, Col_number + 3, commit.hash)
 
-            Row_number += 1
+            if hash_commit in commit.hash[:12]:
+                Col_n = Col_n + 1
+                ws.write(Row_number, Col_number + Col_n, modified_file.filename, italic)
+            else:
+                Col_n = 3
+                Row_number += 1
+                hash_commit = commit.hash[:12]
+                ws.write(Row_number, Col_number, commit.hash[:12])
+                ws.write(Row_number, Col_number + 1, commit.msg)
+                ws.write(Row_number, Col_number + 2, str(commit.author_date))
+                ws.write(Row_number, Col_number + 3, modified_file.filename, italic)
+
+
+
 
     wb.close()
+    print("finish...")
+
+    # load data into a DataFrame object:
+    #df = pd.DataFrame(pd.read_excel(Outputdata))
+
+    #print(df)
+
+
+
 
 
--- setup.py
+++ setup.py
@@ -1,26 +1,22 @@
 import setuptools
-from setuptools import setup, find_packages
-# read the contents of your README file
-# from os import path
-# this_directory = path.abspath (path.dirname (__file__))
-# with open (path.join (this_directory , 'README. md'), encoding ='utf-8') as f:
-#     long_description = f.read
-
+from setuptools import setup
+requires_modules = [
+        'argparse',
+        'pydriller',
+        'xlsxwriter',
+        'datetime'
+        ]
 
 setup(
-    name='myPythonProject',
+    name='pythonProject4',
     version='1.0.0',
-    packages=find_packages(),
-    install_requires=[
-        'pydriller',
-        'xlsxwriter'
-    ],
-    python_requires='>=3.10.5',
-    url='https://github.com/nasr-saab/Nasr_tutorial/new/main',
+    install_requires=requires_modules,
+    packages=setuptools.find_packages(),
+    url='https://github.com/nasr-saab/Nasr_tutorial',
     license='',
-    author='nasrsaab',
-    author_email='nasr.saab@gmail.com',
-    description='git commit from the web sit and show it in the command line',
-    py_modules=['parser_module'],
-
+    author='nasr saab',
+    author_email='nsaab@nvidia.com',
+    description='Write all the commits in Excel file',
+    python_requre='>=3',
+    py_modules=['parser_module']
 )
