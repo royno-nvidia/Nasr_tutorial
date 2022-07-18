60485ff684a37ba43c51547e41645793f4d45852
Nasr Saab <nsaab@nvidia.com>
2022-07-18 15:15:05 +0300

    Expandable code

--- git_parser.py
+++ git_parser.py
@@ -6,18 +6,18 @@ from datetime import date, timedelta,datetime
 from pydriller import Repository
 from pydriller.domain.commit import ModificationType
 import xlsxwriter as xw
-import argparse
 
 
 
 
-def show_commit(output_file,repository,show_lats_days):
+
+def show_commit(args):
 
     #Create a output file
-    if output_file:
+    if args.output_file:
         #Choose a new local path
-        wb = xw.Workbook(output_file +"\outputExcel_file.xlsx")
-        print("\nExcel file created in: " + output_file + "\outputExcel_file.xlsx")
+        wb = xw.Workbook(args.output_file +"\outputExcel_file.xlsx")
+        print("\nExcel file created in: " + args.output_file + "\outputExcel_file.xlsx")
     else:
         #Set in a same arg_parser.py path
         wb = xw.Workbook("outputExcel_file.xlsx")
@@ -33,16 +33,16 @@ def show_commit(output_file,repository,show_lats_days):
     #Calculate date
     to_day = datetime.now().today()
 
-    if show_lats_days:
+    if args.show_lats_days:
 
-        first_day = to_day - timedelta(days=show_lats_days)
+        first_day = to_day - timedelta(days=args.show_lats_days)
         print(first_day)
         print(to_day)
 
     else:
         first_day = datetime.min
-        print(first_day)
         print(to_day)
+
 
     print("Please wait...")
     Row_number = 0
@@ -57,7 +57,7 @@ def show_commit(output_file,repository,show_lats_days):
     hash_commit = "xxxx"
     Col_n = 3
 
-    for commit in Repository(path_to_repo=repository, since=first_day, to=to_day).traverse_commits():
+    for commit in Repository(path_to_repo=args.repository, since=first_day, to=to_day).traverse_commits():
         for modified_file in commit.modified_files:
             ws.write(Row_number, Col_number, commit.hash[:12])
             ws.write(Row_number, Col_number + 1, commit.msg)
@@ -74,19 +74,3 @@ def show_commit(output_file,repository,show_lats_days):
     wb.close()
     print("finish...")
 
-if __name__=='__main__':
-    # Create the parser
-    parser = argparse.ArgumentParser(description='Parser commit from any repository path')
-
-    # Add Arguments
-    parser.add_argument('-o', '--output_file', type=str, default=False,
-                        help='Set a local computer path to create an Excel file')
-    parser.add_argument('-r', '--repository', type=str, metavar='', required=True,
-                        help='Set the repository path you want to parse it')
-    parser.add_argument('-d', '--show_lats_days', type=int, default=False,
-                        help='show me only commits from the last \'X\' days.if not provided -'
-                             ' show all commits In the repository.')
-    # Parse the argument
-    args = parser.parse_args()
-
-    show_commit(args.output_file, args.repository,args.show_lats_days)
