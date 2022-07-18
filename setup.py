abe8ed3a38db239a29e605172b38bb1d19f986a9
Nasr Saab <nsaab@nvidia.com>
2022-07-18 15:15:05 +0300

    New packages in setup file beacause I used date a getparse packages

--- setup.py
+++ setup.py
@@ -1,26 +1,19 @@
-import setuptools
 from setuptools import setup, find_packages
-# read the contents of your README file
-# from os import path
-# this_directory = path.abspath (path.dirname (__file__))
-# with open (path.join (this_directory , 'README. md'), encoding ='utf-8') as f:
-#     long_description = f.read
-
+import setuptools
 
 setup(
-    name='myPythonProject',
-    version='1.0.0',
+    name='Git_parser_v3',
+    version='3.0.0',
     packages=find_packages(),
     install_requires=[
         'pydriller',
-        'xlsxwriter'
+        'xlsxwriter',
+        'argparse',
+        'datetime'
     ],
-    python_requires='>=3.10.5',
-    url='https://github.com/nasr-saab/Nasr_tutorial/new/main',
+    url='https://github.com/nasr-saab/Nasr_tutorial',
     license='',
-    author='nasrsaab',
-    author_email='nasr.saab@gmail.com',
-    description='git commit from the web sit and show it in the command line',
-    py_modules=['git_parser'],
-
+    author='nasr saab',
+    author_email='nsaab@nvidia.com',
+    description='git parser project'
 )
